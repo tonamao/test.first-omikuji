b6f464e74224c19c5f85c651537a2b2a84232ec9
shimba_m <mao.shimba.tona@gmail.com>
2019-01-29 01:17:23 +0900

    upgrade to img version omikuji

--- js/omikuji-main.js
+++ js/omikuji-main.js
@@ -2,11 +2,16 @@
 	'use strict';
 	var btn = document.getElementById('btn');
 	var result = document.getElementById('result');
+	var rslt = document.getElementById('rslt');
 
 	btn.addEventListener('click', function() {
-      var results = ['大吉', '中吉', '小吉', '凶'];
-      var randomNum = Math.floor(Math.random() * results.length)
-      result.textContent = results[randomNum];
+      var imglist = [
+    	  "img/omikuji_daikichi.png",
+    	  "img/omikuji_kichi.png",
+    	  "img/omikuji_kyou.png"];
+      var randomNum = Math.floor(Math.random() * imglist.length);
+      var element = '<img src="' + imglist[randomNum] + '" alt = "" />'
+      rslt.innerHTML = element;
 	});
 
 	btn.addEventListener('mousedown', function() {
