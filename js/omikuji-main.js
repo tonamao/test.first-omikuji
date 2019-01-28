ac216f0f3538e7d9a3630ae70c7e896c98d220db
shimba_m <mao.shimba.tona@gmail.com>
2019-01-28 23:55:12 +0900

    add a button to click

--- js/omikuji-main.js
+++ js/omikuji-main.js
@@ -1,16 +1,18 @@
 (function() {
 	'use strict';
-
 	var btn = document.getElementById('btn');
+	var result = document.getElementById('result');
 
 	btn.addEventListener('click', function() {
-		var results = ['大吉', '中吉', '小吉', '凶'];
-	    var randomNum = Math.floor(Math.random() * results.length)
-	    this.textContent = results[randomNum];
+      var results = ['大吉', '中吉', '小吉', '凶'];
+      var randomNum = Math.floor(Math.random() * results.length)
+      result.textContent = results[randomNum];
 	});
+
 	btn.addEventListener('mousedown', function() {
 		this.className = 'pushed';
 	});
+
 	btn.addEventListener('mouseup', function() {
 		this.className = '';
 	});
